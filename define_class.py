a2d93a0df096e78c7bfbc87edf19721c5ae48964
pskulkarni7 <pallaviskulkarni7@gmail.com>
2025-06-24 14:57:07 +0800

    user class

---
+++ define_class.py
@@ -0,0 +1,9 @@
+class User:
+    '''The clas defining a user'''  
+    def __init__(self):
+        '''Initialises the class User'''
+        print("Hello from the user class")
+
+Pallavi = User()
+Matt = User()
+Ryan = User()
