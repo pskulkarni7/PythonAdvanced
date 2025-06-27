194543812b31d95c34e8189526ab0de0804fcc48
pskulkarni7 <pallaviskulkarni7@gmail.com>
2025-06-27 10:27:02 +0800

    Added function full_name

--- define_class.py
+++ define_class.py
@@ -1,9 +1,26 @@
 class User:
     '''The clas defining a user'''  
-    def __init__(self):
+    def __init__(self, fname, lname):
+        
         '''Initialises the class User'''
+        self.name = ""
+        self.fname = fname
+        self.lname = lname
+
         print("Hello from the user class")
 
-Pallavi = User()
-Matt = User()
-Ryan = User()
+    def full_name(self):
+        '''Prints full name of the user'''
+        self.name = self.fname + " " + self.lname
+
+
+pallavi = User("Pallavi" , "Kulkarni")
+pallavi.full_name()
+print(pallavi.name)
+matt = User("Matt" , "Delac")
+matt.full_name()
+print(matt.name)
+pallavi.lname = "Deshpande"
+pallavi.full_name()
+print(pallavi.name)
+
