4642e5783eaee8320949bd55d5427bc4fdf1d1f2
pskulkarni7 <pallaviskulkarni7@gmail.com>
2025-10-01 10:14:18 +0800

    Inheritance

---
+++ greetuser.py
@@ -0,0 +1,8 @@
+from user1 import User, FrenchUser, SpanishUser
+
+Maria = User("Maria")
+Maria.greet()   
+Jean = FrenchUser("Jean")
+Jean.greet()            
+Carlos = SpanishUser("Carlos")
+Carlos.greet()
